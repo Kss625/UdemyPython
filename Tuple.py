c0424b318a3191800ffbf9c82ce20e2faf3e9812
Kss625 <projecty625@gmail.com>
2023-11-13 14:07:00 +0530

    python

--- Tuple.py
+++ Tuple.py
@@ -21,20 +21,41 @@
 # print(country_copy)
 
 '''Unpacking Tuples'''
-a = b = c = d = 10
-print(a, b, c, d)
-x, y, z = 10, 20, 30
-print(x, y, z)
-# list me bhi aisa hota hain
+# a = b = c = d = 10
+# print(a, b, c, d)
+# x, y, z = 10, 20, 30
+# print(x, y, z)
+# # list me bhi aisa hota hain
+#
+# my_tuple = ("one", "two", "three", "four", "five", "six")
+# for i in my_tuple:
+#     print(i)
+# print()
+# for i in range(len(my_tuple)):
+#     print(i, my_tuple[i])
+# print()
+# '''new way '''
+# for index, value in enumerate(my_tuple):  # by default 0
+#     print(index, value)
 
-my_tuple = ("one", "two", "three", "four", "five", "six")
-for i in my_tuple:
-    print(i)
-print()
-for i in range(len(my_tuple)):
-    print(i, my_tuple[i])
-print()
-'''new way '''
-for index, value in enumerate(my_tuple):  # by default 0
-    print(index, value)
-print(5+10)# just for checking
+
+'''
+Even Index with Enumerate
+Implement a function which takes as a parameter a tuple and return a new tuple but only have even index elements from original tuple.
+
+Hint : Use enumerate() function
+'''
+
+
+def even_index_items(p_tuple):
+    # TODO
+    new_tuple = tuple()
+    for index, value in enumerate(p_tuple):
+        if index % 2 == 0:
+            new_tuple = new_tuple + tuple(p_tuple[index])
+
+    return new_tuple
+
+
+my_tuple = ("a", "b", "c", "d", "e", "f", "g")
+print(even_index_items(my_tuple))
