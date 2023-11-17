b4846daf9c1e45983a3b31141ed5c7959d6cad8d
Kss625 <projecty625@gmail.com>
2023-11-17 16:39:04 +0530

    python

--- projects/Project6-grossPlaywithOvertime.py
+++ projects/Project6-grossPlaywithOvertime.py
@@ -1,8 +1,8 @@
 Hours = float(input("Enter Hours:"))
 rate = float(input("Enter Rate:"))
 # new rate for overtime is 1.5
+pay1 = 40 * rate
 if Hours > 40:
-    pay1 = 40 * rate
     h1 = Hours - 40
     pay2 = h1 * rate * 1.5
     print(f"Total Pay:{round(pay1 + pay2, 2)}")
--- projects/Projects1-Greetings.py
+++ projects/Projects1-Greetings.py
@@ -1,3 +1,3 @@
 name = input("Enter Your name-")
 print("Hello", name)  # print("Hello "+name),print(f"Hello {name} ")
-print("Welcome To The Python")
+print("Welcome To The Python Programming World")
